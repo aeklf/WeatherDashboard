6f34dea8b60fc7b2ddaa36afc35432d98cc4b67b
Andrea Kalife <andreaklfdlg@gmail.com>
2022-04-30 02:24:18 -0500

    JS First Functions

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,29 @@ function findCity() {
         if (response.ok) {
             response.json().then(function (data) {
                 $("#cityName")[0].textContent = cityName + "(" + moment().format("DD/MM/YYYY") + ")";
-            }
-            )
+                $("#cityList").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action cityName">' + cityName);
+           
+                const lat = data.coord.lat;
+                const lon = data.coord.lon;
+
+                var latLonPair = lat.toString() + "" + lon.toString();
+
+                localStorage.setItem(cityName, latLonPair);
+
+                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+
+                fetch(apiURL).then(function (newResponse){
+                    if (newResponse.ok) {
+                        newResponse.json().then(function (newData) {
+                            getCurrentWeather(newData);
+                        })
+                    }
+                })
+            })
+        } else {
+            alert("Place Not Found x.x");
         }
-    }
-    )
-}
+    })
+} 
+
+
